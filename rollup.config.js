ee74ea9c1716b0d4e55df6b32be6444b02a736e2
Ryan Park <admin@ryanpark.co.uk>
2022-09-15 20:27:05 +0100

    swc

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
+// import typescript from "@rollup/plugin-typescript";
+import swc from "rollup-plugin-swc3";
 import dts from "rollup-plugin-dts";
 import { terser } from "rollup-plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
@@ -26,14 +27,14 @@ export default [
       peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
+      swc({ sourceMaps: true }),
       terser(),
     ],
-    external: ["react", "react-dom", "styled-components"],
+    external: ["react", "react-dom"],
   },
   {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    input: "src/index.ts",
+    output: [{ file: "dist/types/index.d.ts", format: "esm" }],
     plugins: [dts()],
   },
 ];
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export * as CharacterPane from "./components/CharacterPane";
