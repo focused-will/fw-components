706af889158efca6bee2dedbfdb30bebdf8c42ab
Ryan Park <admin@ryanpark.co.uk>
2022-10-02 20:52:30 +0100

    fix storybook path alias support

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,11 @@ const path = require("path");
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
   framework: "@storybook/react",
   typescript: {
     check: false,
@@ -15,6 +19,12 @@ module.exports = {
   },
   webpackFinal: async (config) => {
     config.resolve.modules = [...(config.resolve.modules || []), path.resolve(__dirname, "../src")];
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "../src"),
+    };
+
     return config;
   },
 };
